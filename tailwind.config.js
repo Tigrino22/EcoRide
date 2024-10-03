3bf3ebff02e594e1fc1d106aa51d6b71c2f8d088
Tigrino22 <valentin.legris@outlook.com>
2024-10-03 10:45:30 +0200

    configuration de tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './Templates/**/*.html.twig',
+      './assets/**/*.ts'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
